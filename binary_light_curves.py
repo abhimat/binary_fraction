255d6cef7bef76181c9785a945e3f82380ddeb41
Abhimat Gautam <abhimat.gautam@gmail.com>
2022-06-13 20:48:37 -0700

    Fixes to time phasing for mock light curves

--- binary_light_curves.py
+++ binary_light_curves.py
@@ -17,12 +17,15 @@ from phoebe import c as const
 from tqdm import tqdm
 
 # Function to help with parallelization
-def binary_light_curve_from_binary_row(cur_binary_row, bin_pop_lc_obj, out_dir):
+def binary_light_curve_from_binary_row(cur_binary_row, bin_pop_lc_obj,
+                                       use_blackbody_atm,
+                                       out_dir):
     bin_pop_lc_obj.make_binary_light_curve(cur_binary_row['binary_index'],
                        cur_binary_row['mass_1'], cur_binary_row['mass_2'],
                        cur_binary_row['binary_period'], cur_binary_row['binary_t0_shift'],
                        cur_binary_row['binary_q'], cur_binary_row['binary_ecc'],
                        cur_binary_row['binary_inc'],
+                       use_blackbody_atm=use_blackbody_atm,
                        out_dir=out_dir)
 
 # Class for generating light curves in a binary population
@@ -98,10 +101,13 @@ class binary_pop_light_curves(object):
     def make_binary_light_curve(self, binary_index, mass_1, mass_2,
                                 binary_period, binary_t0_shift,
                                 binary_q, binary_ecc, binary_inc,
+                                use_blackbody_atm=False,
                                 out_dir='./mock_binaries'):
         # Make sure output directory exists
         if not os.path.exists(out_dir):
             os.makedirs(out_dir)
+        
+        # print(binary_index)
         
         # Interpolate stellar parameters from isochrone
         (star1_params_all, star1_params_lcfit) = self.pop_isochrone.mass_init_interp(mass_1)
@@ -120,29 +126,48 @@ class binary_pop_light_curves(object):
         # Obtain model magnitudes
         model_success = True
         
-        try:
-            num_triangles = 500
-            (binary_model_mags_Kp, binary_model_mags_H) = lc_calc.binary_mags_calc(
-                                                              star1_params_lcfit,
-                                                              star2_params_lcfit,
-                                                              binary_params_model,
-                                                              model_times,
-                                                              self.isoc_ext,
-                                                              self.ext_Kp, self.ext_H,
-                                                              self.ext_alpha,
-                                                              self.isoc_dist * u.pc,
-                                                              self.pop_distance,
-                                                              use_blackbody_atm=False,
-                                                              num_triangles=num_triangles)
-        except:
+        binary_model_mags_Kp = np.empty(len(self.obs_times_Kp))
+        binary_model_mags_H = np.empty(len(self.obs_times_H))
+        
+        num_triangles = 500
+        binary_model_out = lc_calc.binary_mags_calc(
+            star1_params_lcfit,
+            star2_params_lcfit,
+            binary_params_model,
+            model_times,
+            self.isoc_ext,
+            self.ext_Kp, self.ext_H,
+            self.ext_alpha,
+            self.isoc_dist * u.pc,
+            self.pop_distance,
+            use_blackbody_atm=use_blackbody_atm,
+            num_triangles=num_triangles)
+        
+        if binary_model_out == -np.inf:
             model_success = False
             (binary_model_mags_Kp, binary_model_mags_H) = ([-1], [-1])
+        else:
+            (binary_model_out_Kp, binary_model_out_H) = binary_model_out
             
+            # Calculate model time phasing
+            (kp_phase_out, h_phase_out) = lc_calc.phased_obs(
+                                              model_times,
+                                              binary_period * u.d, t0)
+    
+            (kp_phased_days, kp_phases_sorted_inds,
+             kp_model_times) = kp_phase_out
+            (h_phased_days, h_phases_sorted_inds,
+             h_model_times) = h_phase_out
+        
+            # Place output model mags into correctly phased slots for mags
+            binary_model_mags_Kp[kp_phases_sorted_inds] = binary_model_out_Kp
+            binary_model_mags_H[h_phases_sorted_inds] = binary_model_out_H
+                
         
         # Save out binary light curve
         ## Make sure output directory exists
-        if not os.path.exists(out_dir + '/binary_light_curves'):
-            os.makedirs(out_dir + '/binary_light_curves')
+        if not os.path.exists(out_dir + '/model_light_curves'):
+            os.makedirs(out_dir + '/model_light_curves')
         
         ## Save out model magnitudes
         if model_success:
@@ -150,13 +175,16 @@ class binary_pop_light_curves(object):
                                          names=('MJD', 'mags_Kp'))
             binary_mags_H_table = Table([self.obs_times_H, binary_model_mags_H],
                                         names=('MJD', 'mags_H'))
-        
+            
+            binary_mags_Kp_table['mags_Kp'].info.format = '.6f'
+            binary_mags_H_table['mags_H'].info.format = '.6f'
+            
             binary_mags_Kp_table.write('{0}/binary_{1}_mags_Kp.txt'.format(
-                                            out_dir + '/binary_light_curves', int(binary_index)),
+                                            out_dir + '/model_light_curves', int(binary_index)),
                                        overwrite=True, format='ascii.fixed_width')
-        
+            
             binary_mags_H_table.write('{0}/binary_{1}_mags_H.txt'.format(
-                                            out_dir + '/binary_light_curves', int(binary_index)),
+                                            out_dir + '/model_light_curves', int(binary_index)),
                                       overwrite=True, format='ascii.fixed_width')
         else:
             binary_mags_Kp_table = Table([[-1], binary_model_mags_Kp],
@@ -165,17 +193,18 @@ class binary_pop_light_curves(object):
                                         names=('MJD', 'mags_H'))
         
             binary_mags_Kp_table.write('{0}/binary_{1}_mags_Kp.txt'.format(
-                                            out_dir + '/binary_light_curves', int(binary_index)),
+                                            out_dir + '/model_light_curves', int(binary_index)),
                                        overwrite=True, format='ascii.fixed_width')
         
             binary_mags_H_table.write('{0}/binary_{1}_mags_H.txt'.format(
-                                            out_dir + '/binary_light_curves', int(binary_index)),
+                                            out_dir + '/model_light_curves', int(binary_index)),
                                       overwrite=True, format='ascii.fixed_width')
             
         ## Return model magnitudes
         return (binary_model_mags_Kp, binary_model_mags_H)
     
     def make_binary_population_light_curves(self, binary_pop_params_file,
+                                            use_blackbody_atm=False,
                                             out_dir='./mock_binaries',
                                             parallelize=True):
         ## Read in table of binary parameters
@@ -184,7 +213,7 @@ class binary_pop_light_curves(object):
         ## Generate light curves for all binaries
         import parmap
         parmap.map(binary_light_curve_from_binary_row, binary_pop_params_table,
-                   self, out_dir=out_dir,
+                   self, use_blackbody_atm=use_blackbody_atm, out_dir=out_dir,
                    pm_pbar=True, pm_parallel=parallelize)
         
         return
