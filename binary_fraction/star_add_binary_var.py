3c4d1046a825a37edeec3c65ca4831c7ece28e68
Abhimat Gautam <abhimat.gautam@gmail.com>
2022-10-20 17:34:02 -0700

    Updated code for adding mock light curve sampling

--- binary_fraction/star_add_binary_var.py
+++ binary_fraction/star_add_binary_var.py
@@ -5,7 +5,6 @@
 import numpy as np
 from astropy.table import Table
 from gc_photdata import align_dataset
-from binary_fraction.obs_light_curves import add_obs_uncs
 import os
 
 class star_add_binary_var(object):
@@ -18,25 +17,52 @@ class star_add_binary_var(object):
         
         return
     
-    def load_binary_obs_params_table(
-            self, table_file_path='./binary_obs_params.h5'):
+    def set_model_lcs_dir(self, model_lcs_dir):
         """
-        Load the binary observation parameters table
+        Set model_lcs_dir variable for class, where model light curves for
+        mock binaries are located
+        
+        Parameters
+        ----------
+        model_lcs_dir : str, default: './mock_binaries/model_light_curves/'
+            Directory where the mock binary model light curves are stored
+        """
+        self.model_lcs_dir = model_lcs_dir
+    
+    def load_model_lc_params_table(
+            self, table_file_path='./binary_model_lc_params.h5'):
+        """
+        Load the binary model light curve parameters table
         """
         # Load in the table
-        binary_obs_params_table = Table.read(
+        model_lc_params_table = Table.read(
             table_file_path, path='data')
         
-        # Clean out stars that don't have any light curves
-        lc_filt = np.where(binary_obs_params_table['num_nights_kp'] > 0)
+        model_lc_params_table.add_index(['binary_index'])
         
-        binary_obs_params_table = binary_obs_params_table[lc_filt]
+        # # Clean out stars that don't have any light curves
+        # lc_filt = np.where(model_lc_params_table['med_mag_kp'] > 0)
+        #
+        # model_lc_params_table = model_lc_params_table[lc_filt]
         
         # Save out as a class variable
-        self.binary_obs_params_table = binary_obs_params_table
+        self.model_lc_params_table = model_lc_params_table
         
-        self.num_nights_kp = int(binary_obs_params_table['num_nights_kp'][0])
-        self.num_nights_h = int(binary_obs_params_table['num_nights_h'][0])
+        return
+    
+    def load_model_sb_params_table(
+            self, table_file_path='./stellar_binary_params.h5'):
+        """
+        Load the binary model light curve parameters table
+        """
+        # Load in the table
+        model_sb_params_table = Table.read(
+            table_file_path, path='data')
+        
+        model_sb_params_table.add_index(['binary_index'])
+        
+        # Save out as a class variable
+        self.model_sb_params_table = model_sb_params_table
         
         return
     
@@ -63,6 +89,7 @@ class star_add_binary_var(object):
         
         self.num_stars_kp = len(self.star_names_kp)
         self.num_epochs_kp = len(self.epoch_MJDs_kp)
+        self.num_nights_kp = len(self.epoch_MJDs_kp)
         
         self.mags_kp = self.align_data_kp.star_mags_neighCorr
         self.mag_uncs_kp = self.align_data_kp.star_magErrors_neighCorr
@@ -76,11 +103,17 @@ class star_add_binary_var(object):
         
         self.num_stars_h = len(self.star_names_h)
         self.num_epochs_h = len(self.epoch_MJDs_h)
+        self.num_nights_h = len(self.epoch_MJDs_h)
         
         self.mags_h = self.align_data_h.star_mags_neighCorr
         self.mag_uncs_h = self.align_data_h.star_magErrors_neighCorr
         self.mag_means_h = self.align_data_h.star_magMeans_neighCorr
         
+        # Set experiment-wide t0 as floor of minimum observation date
+        # Used when sampling mock binary light curves
+        self.t0 = np.floor(np.min(
+            [np.min(self.epoch_MJDs_kp), np.min(self.epoch_MJDs_h)]
+        ))
         
         return
     
@@ -93,21 +126,20 @@ class star_add_binary_var(object):
         epoch_unc_tables_kp = {}
         epoch_unc_tables_h = {}
         
-        num_epochs_kp = self.num_nights_kp
-        num_epochs_h = self.num_nights_h
-        
         # Read in each epoch's uncertainties table
-        for cur_epoch_index in range(num_epochs_kp):
+        for cur_epoch_index in range(self.num_nights_kp):
             cur_epoch_table = Table.read(
                 f'{uncs_dir}/Kp/epoch_{cur_epoch_index}.txt',
-                format='ascii.fixed_width')
+                format='ascii.fixed_width'
+            )
             
             epoch_unc_tables_kp[cur_epoch_index] = cur_epoch_table
         
-        for cur_epoch_index in range(num_epochs_h):
+        for cur_epoch_index in range(self.num_nights_h):
             cur_epoch_table = Table.read(
                 f'{uncs_dir}/H/epoch_{cur_epoch_index}.txt',
-                format='ascii.fixed_width')
+                format='ascii.fixed_width'
+            )
             
             epoch_unc_tables_h[cur_epoch_index] = cur_epoch_table
         
@@ -117,9 +149,81 @@ class star_add_binary_var(object):
         
         return
     
+    
+    def draw_bin_mags(self,model_number, phase_shift,
+            print_diagnostics=False,
+        ):
+        """
+        For a model binary of index {model_number}, sample the model light curve
+        at this experiment's observation dates.
+        """
+        # Retrieve relevant information about binary model
+        bin_sb_row = self.model_sb_params_table.loc[model_number]
+        
+        binary_period = bin_sb_row['binary_period']
+        
+        # Read in the model
+        model_mags_kp_table = Table.read(
+            f'{self.model_lcs_dir}/binary_{model_number}_mags_Kp.h5',
+            path='data')
+        
+        model_mags_h_table = Table.read(
+            f'{self.model_lcs_dir}/binary_{model_number}_mags_H.h5',
+            path='data')
+        
+        # Shift the model phases by the phase shift var
+        sel_model_phases_kp = (model_mags_kp_table['model_phases_Kp'] +
+                               phase_shift) % 1.0
+        
+        sel_model_phases_h = (model_mags_h_table['model_phases_H'] +
+                              phase_shift) % 1.0
+        
+        if print_diagnostics:
+            print(sel_model_phases_kp)
+            print(model_mags_kp_table['model_phases_Kp'])
+        
+        # Sort the model tables so that the shifted phases are in order
+        kp_argsort = np.argsort(sel_model_phases_kp)
+        
+        sel_model_phases_kp = sel_model_phases_kp[kp_argsort]
+        model_mags_kp_table = model_mags_kp_table[kp_argsort]
+        
+        h_argsort = np.argsort(sel_model_phases_h)
+        
+        sel_model_phases_h = sel_model_phases_h[h_argsort]
+        model_mags_h_table = model_mags_h_table[h_argsort]
+        
+        if print_diagnostics:
+            print(sel_model_phases_kp)
+            print(model_mags_kp_table['model_phases_Kp'])
+            print(model_mags_kp_table['mags_Kp'])
+        
+        # Draw model mags at observation times
+        
+        # Calculate the observation times' phases
+        obs_phases_kp = (self.epoch_MJDs_kp - self.t0) % binary_period
+        obs_phases_h = (self.epoch_MJDs_h - self.t0) % binary_period
+        
+        # Interpolate the light curves at the observation's phases
+        # from the model
+        model_obs_mags_kp = np.interp(
+            obs_phases_kp,                  # Experiment observation phases
+            sel_model_phases_kp,            # Model phases (shifted)
+            model_mags_kp_table['mags_Kp'], # Model mags (shifted)
+        )
+        
+        model_obs_mags_h = np.interp(
+            obs_phases_h,                   # Experiment observation phases
+            sel_model_phases_h,             # Model phases (shifted)
+            model_mags_h_table['mags_H'],   # Model mags (shifted)
+        )
+        
+        # Return interpolated mags at observation phases
+        return (model_obs_mags_kp, model_obs_mags_h)
+    
+    
     def star_lc_add_binary_var(
-            self, star, num_lcs_generate=1,
-            model_lcs_dir='./mock_binaries/model_light_curves/'):
+            self, star, num_lcs_generate=1):
         """For a given star, add mock binary var light curves
         
         Parameters
@@ -130,8 +234,6 @@ class star_add_binary_var(object):
         num_lcs_generate : int, default: 1
             Number of mock light curves to generate for the target star, with
             mock binary light curves added
-        model_lcs_dir : str, default: './mock_binaries/model_light_curves/'
-            Directory where the mock binary model light curves are stored
         """
         
         # Read in the star's light curves
@@ -143,51 +245,70 @@ class star_add_binary_var(object):
         star_mag_uncs_h = self.mag_uncs_h[star]
         star_mag_mean_h = self.mag_means_h[star]
         
-        # Find all mock binaries that have mean magnitudes within +- 0.25 mags
+        # Calculate median magnitude
+        star_det_filt_kp = np.where(star_mags_kp > 0.)
+        star_det_filt_h = np.where(star_mags_h > 0.)
+        
+        star_mag_med_kp = np.median(star_mags_kp[star_det_filt_kp])
+        star_mag_med_h = np.median(star_mags_h[star_det_filt_h])
+        
+        
+        # Find all mock binaries that have median magnitudes within +- 0.25 mags
         mag_rad = 0.25
         
-        mock_bin_mag_filt = np.where(
+        mod_bin_mag_filt = np.where(
             np.logical_and(
-                self.binary_obs_params_table['mag_mean_kp'] >= star_mag_mean_kp - mag_rad,
-                self.binary_obs_params_table['mag_mean_kp'] <= star_mag_mean_kp + mag_rad,
+                self.model_lc_params_table['med_mag_kp'] >=\
+                    star_mag_med_kp - mag_rad,
+                self.model_lc_params_table['med_mag_kp'] <=\
+                    star_mag_med_kp + mag_rad,
             )
         )
         
-        mock_bins_filt_obs_params = self.binary_obs_params_table[mock_bin_mag_filt]
-        mock_bins_filt_inds = mock_bins_filt_obs_params['binary_index']
+        mod_bins_filt_lc_params = self.model_lc_params_table[mod_bin_mag_filt]
+        mod_bins_filt_inds = mod_bins_filt_lc_params['binary_index']
         
-        while len(mock_bins_filt_inds) < 10:
+        while len(mod_bins_filt_inds) < 10:
             mag_rad = mag_rad + 0.25
             print(f'Increasing mag rad size to {mag_rad}')
         
-            mock_bin_mag_filt = np.where(
+            mod_bin_mag_filt = np.where(
                 np.logical_and(
-                    self.binary_obs_params_table['mag_mean_kp'] >= star_mag_mean_kp - mag_rad,
-                    self.binary_obs_params_table['mag_mean_kp'] <= star_mag_mean_kp + mag_rad,
+                    self.model_lc_params_table['med_mag_kp'] >=\
+                        star_mag_med_kp - mag_rad,
+                    self.model_lc_params_table['med_mag_kp'] <=\
+                        star_mag_med_kp + mag_rad,
                 )
             )
         
-            mock_bins_filt_obs_params = self.binary_obs_params_table[mock_bin_mag_filt]
-            mock_bins_filt_inds = mock_bins_filt_obs_params['binary_index']
+            mod_bins_filt_lc_params = self.model_lc_params_table[mod_bin_mag_filt]
+            mod_bins_filt_inds = mod_bins_filt_lc_params['binary_index']
         
         
         # Select n random binaries from the selected binaries
-        selected_bin_inds = np.random.choice(
-                                mock_bins_filt_inds,
-                                size=num_lcs_generate,
-                                replace=True,
-                            )
+        rng = np.random.default_rng()
         
-        selected_obs_params = mock_bins_filt_obs_params[
-                                np.where(
-                                    np.isin(
-                                        mock_bins_filt_obs_params['binary_index'],
-                                        selected_bin_inds,
-                                    )
-                                )
-                              ]
+        selected_bin_inds = rng.choice(
+            mod_bins_filt_inds,
+            size=num_lcs_generate,
+            replace=True,
+        )
         
-        # For each binary pull out mock light curve
+        selected_bin_phase_shifts = rng.random(
+            size=num_lcs_generate,
+        )
+        
+        selected_lc_params = mod_bins_filt_lc_params[
+              np.where(
+                  np.isin(
+                      mod_bins_filt_lc_params['binary_index'],
+                      selected_bin_inds,
+                  )
+              )
+        ]
+        
+        # For each injected binary: generate a mock light curve at observation
+        # dates with a random phase
         sel_mock_light_curves_kp = np.empty(
                                        (num_lcs_generate,
                                         self.num_nights_kp)
@@ -204,34 +325,35 @@ class star_add_binary_var(object):
         for (cur_bin_ind, model_number) in enumerate(selected_bin_inds):
             # Read in and store mock light curves
             # Kp
-            model_mags_kp_table = Table.read(
-                f'{model_lcs_dir}/binary_{model_number}_mags_Kp.txt',
-                format='ascii.fixed_width')
             
-            sel_mock_light_curves_kp[cur_bin_ind] =\
-                model_mags_kp_table['mags_Kp']
+            draw_bin_mags(
+                model_number, selected_bin_phase_shifts[cur_bin_ind],
+            )
             
-            sel_mock_mean_mags_kp[cur_bin_ind] =\
-                np.mean(sel_mock_light_curves_kp[cur_bin_ind])
             
-            sel_mock_light_curves_kp[cur_bin_ind] =\
-                sel_mock_light_curves_kp[cur_bin_ind] -\
-                sel_mock_mean_mags_kp[cur_bin_ind] 
             
-            # H        
-            model_mags_h_table = Table.read(
-                f'{model_lcs_dir}/binary_{model_number}_mags_H.txt',
-                format='ascii.fixed_width')
+            # Subtract median from light curves and store
             
-            sel_mock_light_curves_h[cur_bin_ind] =\
-                model_mags_h_table['mags_H']
+            # sel_mock_light_curves_kp[cur_bin_ind] =\
+            #     model_mags_kp_table['mags_Kp']
+            #
+            # sel_mock_mean_mags_kp[cur_bin_ind] =\
+            #     np.mean(sel_mock_light_curves_kp[cur_bin_ind])
+            #
+            # sel_mock_light_curves_kp[cur_bin_ind] =\
+            #     sel_mock_light_curves_kp[cur_bin_ind] -\
+            #     sel_mock_mean_mags_kp[cur_bin_ind]
             
-            sel_mock_mean_mags_h[cur_bin_ind] =\
-                np.mean(sel_mock_light_curves_h[cur_bin_ind])
             
-            sel_mock_light_curves_h[cur_bin_ind] =\
-                sel_mock_light_curves_h[cur_bin_ind] -\
-                sel_mock_mean_mags_h[cur_bin_ind]
+            # sel_mock_light_curves_h[cur_bin_ind] =\
+            #     model_mags_h_table['mags_H']
+            #
+            # sel_mock_mean_mags_h[cur_bin_ind] =\
+            #     np.mean(sel_mock_light_curves_h[cur_bin_ind])
+            #
+            # sel_mock_light_curves_h[cur_bin_ind] =\
+            #     sel_mock_light_curves_h[cur_bin_ind] -\
+            #     sel_mock_mean_mags_h[cur_bin_ind]
         
         # Make mock light curves, with star lc + model mags
         
@@ -390,7 +512,6 @@ class star_add_binary_var(object):
     
     def create_star_binary_var_table(
             self, star, num_lcs_generate=1,
-            model_lcs_dir='./mock_binaries/model_light_curves/',
             star_bin_var_out_dir='./star_bin_var/',
             print_diagnostics=False):
         """
@@ -404,8 +525,6 @@ class star_add_binary_var(object):
         num_lcs_generate : int, default: 1
             Number of mock light curves to generate for the target star, with
             mock binary light curves added
-        model_lcs_dir : str, default: './mock_binaries/model_light_curves/'
-            Directory where the mock binary model light curves are stored
         star_bin_var_out_dir : str, default: './star_bin_var/'
             Directory where the output tables live for each star,
             with mock binary light curves injected
@@ -419,8 +538,8 @@ class star_add_binary_var(object):
         
         # Generate the mock light curves
         star_lc_add_binary_var_out = self.star_lc_add_binary_var(
-            star, num_lcs_generate=num_lcs_generate,
-            model_lcs_dir=model_lcs_dir)
+            star, num_lcs_generate=num_lcs_generate
+        )
         
         (selected_bin_ids,
          star_mock_light_curves_kp,
